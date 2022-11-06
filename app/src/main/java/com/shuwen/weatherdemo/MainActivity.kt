2641a8f4d8a7251bca7bdf5d386281736308f3e1
shuwen <1461011514@qq.com>
2022-11-06 14:11:20 +0800

    初始化项目

---
+++ app/src/main/java/com/shuwen/weatherdemo/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.shuwen.weatherdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
